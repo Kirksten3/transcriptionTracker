31d4d1108bc1988b654235765fface4b51f47672
Kirk Stennett <Kirk Stennett>
2016-05-10 14:44:14 -0700

    updates on classes including versioning function

--- main.py
+++ main.py
@@ -5,10 +5,22 @@ from time import sleep
 
 class Word:
     def __init__(self):
+        """
+        Word.__init__:
+            This initialization creates a Word object which contains
+            details about the document and Microsoft Word. Including
+            the version, the document, and the Word context from
+            pyWin32
+
+        Returns:
+            A Word Object containing, word context, word version,
+            and the document.
+        """
         self.Word = win32.gencache.EnsureDispatch('Word.Application')
         self.Version = self.Word.Version
+        self.InitializeFunction = {'14.0': Word2010,
+                                   '15.0': Word2013}.get(self.Version, Word2010)
         self.Doc = self.Word.Documents.Add()
-        #make visible
         self.Word.Visible = True
     def Quit(self):
         self.Word.Application.Quit()
@@ -17,12 +29,49 @@ class Word:
         self.Quit()
 
 class Selection:
-    """Must be a Word Selection Object in self.Selection"""
     def __init__(self, selectionObject):
-        self.Selection = selectionObject
-    def ChangeFont(self, font=False, fontSize, fontStyle):
-        self.Selection.Font.Name = font
+        """
+        Selection.__init__:
+            This creates a Selection object, which is simply a wrapper for
+            the VBA word selection object.
 
+        Args:
+            selectionObject (VBA Selection Object): This should be a range of
+                text that has been selected in the word document. Text is
+                selected through use of the .Select() function
+
+        Returns: A Wrapped Selection Object
+        """
+
+        self.Selection = selectionObject
+    def ChangeFont(self, font="", fontSize=None, fontStyle=(False,False)):
+        """
+        ChangeFont:
+            This function modifies a selection object that has been
+            set on initialization, with the following arguments
+
+        Args:
+            font (string): A font to select, e.g. 'Times New Roman'
+            fontSize (int): A size to make the font
+            fontStyle (bool tuple): Specifies whether to use bold and italics
+                e.g. (True, False): use bold not italics
+
+        Returns:
+            void: Simply modifies the text in the selection of the word document
+        """
+        if font is not "":
+            self.Selection.Font.Name = font
+        if fontSize is not None:
+            self.Selection.Font.Size = fontSize
+        self.Selection.Font.Bold = fontStyle[0]
+        self.Selection.Font.Italics = fontStyle[1]
+
+
+def Word2010(doc):
+    print("Word 2010")
+
+def Word2013(doc):
+    print("Word 2013")
 
 def initialize_document(doc, version):
     """Initialize and open Microsoft Word, with specified title"""
@@ -56,7 +105,9 @@ def main():
     #new word object
     word = Word()
 
-    initialize_document(word.Doc, word.Version)
+    word.InitializeFunction(word.Doc)
+
+    #initialize_document(word.Doc, word.Version)
 
     word.QuitNoSave()
 
