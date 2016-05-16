19edd47cdad85a902f0b48a5375f0345d357470a
Kirk <kjsstennett@gmail.com>
2016-05-16 15:52:23 -0700

    built out a few functions, UI partially works

--- main.py
+++ main.py
@@ -25,13 +25,18 @@ class Word:
         """
         self.word = win32.gencache.EnsureDispatch('Word.Application')
         self.version = self.word.Version
-        self.initialize_function = {'14.0': self.word2010,
-                                    '15.0': self.word2013,
-                                    '16.0': self.word2016}.get(self.version, self.word2010)
+        self.initialize_function = {'14.0': self.Word2010,
+                                    '15.0': self.Word2013,
+                                    '16.0': self.Word2016}.get(self.version, self.Word2010)
         self.doc = self.word.Documents.Add()
         self.word.Visible = True
 
-    def word2010(self, details):
+    @staticmethod
+    def Start_Word(details):
+        word = Word()
+        word.initialize_function(details)
+
+    def Word2010(self, details):
         # take course name, lesson and video name
         doc_start = self.doc.Range(0, 0)
         #this should be replaced with actual course info
@@ -58,18 +63,18 @@ class Word:
 
         sleep(5)
 
-    def word2013(self, details):
-        self.word2010(details)
+    def Word2013(self, details):
+        self.Word2010(details)
 
-    def word2016(self, details):
+    def Word2016(self, details):
         x=5
 
-    def quit(self):
+    def Quit(self):
         self.word.Application.Quit()
 
-    def quit_no_save(self):
+    def Quit_No_Save(self):
         self.doc.Close(False)
-        self.quit()
+        self.Quit()
 
 class Word_Details:
     def __init__(self, course_name, week, video_name, num_slides):
@@ -79,7 +84,7 @@ class Word_Details:
         self.Slides = num_slides
 
 class UI:
-    def __init__(self):
+    def __init__(self, is_timer=False):
         """
         A generic UI class that will handle the new/open menu,
         the creation menu, and the timer menu for use
@@ -91,6 +96,16 @@ class UI:
         """
         self.Root = tk.Tk()
         self.Root.title("Transcription Tracker")
+        # on close, hide window
+        self.Root.protocol("WM_DELETE_WINDOW", self.Root.iconify)
+
+        # create menu to allow user to exit, use for timer window
+        menu_bar = tk.Menu(self.Root)
+        file_menu = tk.Menu(menu_bar, tearoff=0)
+        file_menu.add_command(label="Exit", command=self.Destroy)
+        menu_bar.add_cascade(label="File", menu=file_menu)
+        self.Root.config(menu=menu_bar)
+
         self.Mainframe = ttk.Frame(self.Root, padding="3 3 12 12")
         self.Mainframe.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
         self.Mainframe.columnconfigure(0, weight=1)
@@ -100,17 +115,19 @@ class UI:
         self.Root.destroy()
 
 class Menu(UI):
+    # holds the Word_Details class info
+    Details = ""
 
     @staticmethod
-    def New_Or_Open(self):
+    def New_Or_Open():
         ui = UI()
         # button initialization
         ttk.Label(ui.Mainframe, text="Create a new document, or open a current document.")\
             .grid(column=2, row=1, sticky=tk.N)
 
-        ttk.Button(ui.Mainframe, text="New", command=self.Destroy_And_Call(ui, self.New_Document))\
+        ttk.Button(ui.Mainframe, text="New", command= lambda: Menu.Destroy_And_Call(ui, Menu.New_Document))\
             .grid(column=2, row=2, sticky=tk.E)
-        ttk.Button(ui.Mainframe, text="Open", command=self.Destroy_And_Call(ui, self.Open_Document))\
+        ttk.Button(ui.Mainframe, text="Open", command= lambda: Menu.Destroy_And_Call(ui, Menu.Open_Document))\
             .grid(column=3, row=2, sticky=tk.W)
 
         for child in ui.Mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
@@ -118,33 +135,101 @@ class Menu(UI):
         ui.Root.mainloop()
 
     @staticmethod
-    def Open_Document(self):
+    def Open_Document():
         x=5
 
     @staticmethod
-    def New_Document(self):
-        x=5
+    def New_Document():
+        ui = UI()
+
+        # variable definitions
+        course_name = tk.StringVar()
+        week = tk.StringVar()
+        video_name = tk.Text(ui.Mainframe, height=3, width=40)
+        num_slides = tk.StringVar()
+
+        # creating entries
+        course_name_entry = ttk.Entry(ui.Mainframe, width=12, textvariable=course_name)
+        week_entry = ttk.Entry(ui.Mainframe, width=3, textvariable=week)
+        num_slides_entry = ttk.Entry(ui.Mainframe, width=3, textvariable=num_slides)
+
+        # aligning on grid
+        course_name_entry.grid(column=2, row=1, sticky=tk.W)
+        week_entry.grid(column=2, row=2, sticky=tk.W)
+        video_name.grid(column=2, row=3)
+        num_slides_entry.grid(column=2, row=5, sticky=tk.W)
+
+        ttk.Button(ui.Mainframe, text="Go",
+                   command=lambda: Menu.Validate_Word_Details(ui,
+                                                          Menu.Timer,
+                                                          Menu.New_Document,
+                                                          Word_Details(course_name.get(),
+                                                                       week.get(),
+                                                                       video_name.get("1.0", tk.END),
+                                                                       num_slides.get())))\
+            .grid(column=3, row=6, sticky=tk.E)
+
+        #set up labels for the ui
+        ttk.Label(ui.Mainframe, text="Course Name:").grid(column=1, row=1, sticky=tk.W)
+        ttk.Label(ui.Mainframe, text="*", foreground="red").grid(column=1, row=1, sticky=tk.E)
+        ttk.Label(ui.Mainframe, text="Week Number:").grid(column=1, row=2, sticky=tk.W)
+        ttk.Label(ui.Mainframe, text="Video Name:").grid(column=1, row=3, sticky=tk.W)
+        ttk.Label(ui.Mainframe, text="*", foreground="red").grid(column=1, row=3, sticky=tk.E)
+        ttk.Label(ui.Mainframe, text="Number of Slides:").grid(column=1, row=5, sticky=tk.W)
+
+        ttk.Label(ui.Mainframe, text="Required *", foreground="red").grid(column=1, row=6, sticky=tk.W)
+
+        for child in ui.Mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
+
+        course_name_entry.focus()
+        # ui.Root.bind('<Return>', main)
+
+        ui.Root.mainloop()
+
 
     @staticmethod
-    def Timer(self):
-        x=5
+    def Timer():
+        ui = UI()
+
+        #static "init" function
+        Word.Start_Word(Menu.Details)
+
 
     @staticmethod
-    def Destroy_And_Call(self, ui, function):
+    def Validate_Word_Details(ui, function, calling_function, details):
+
+        # loads word details into static member variable and calls specified function
+        Menu.Details = details
+
+        if details.Course == "":
+            Menu.Destroy_And_Call(ui, calling_function)
+        if details.Video == "":
+            Menu.Destroy_And_Call(ui, calling_function)
+        if not details.Week.isnumeric() and details.Week != "":
+            Menu.Destroy_And_Call(ui, calling_function)
+        if not details.Slides.isnumeric() and details.Slides != "":
+            Menu.Destroy_And_Call(ui, calling_function)
+
+        Menu.Destroy_And_Call(ui, function)
+
+    @staticmethod
+    def Destroy_And_Call(ui, function):
         ui.Destroy()
         function()
 
-def main(details):
-    #arg is a Word_Details object
 
-    #new word object
-    word = Word()
 
-    word.initialize_function(details)
-
-    #initialize_document(word.Doc, word.Version)
-
-    #word.quit_no_save()
+# def Start_Word(details):
+#     #arg is a Word_Details object
+#
+#     #new word object
+#     word = Word()
+#
+#     word.initialize_function(details)
+#
+#     #initialize_document(word.Doc, word.Version)
+#
+#     #word.quit_no_save()
 
 # def ui():
 #     root = Tk()
@@ -168,38 +253,14 @@ def main(details):
 #     # text_area.pack(expand=YES, fill=BOTH)
 #
 #     #creating entry text boxes
-#     course_name_entry = ttk.Entry(mainframe, width=12, textvariable=course_name)
-#     week_entry = ttk.Entry(mainframe, width=3, textvariable=week)
-#     num_slides_entry = ttk.Entry(mainframe, width=3, textvariable=num_slides)
-#
-#     #aligning on grid
-#     course_name_entry.grid(column=2, row=1, sticky=W)
-#     week_entry.grid(column=2, row=2, sticky=W)
-#     video_name.grid(column=2, row=3)
-#     num_slides_entry.grid(column=2, row=5, sticky=W)
+
 #
 #
-#     ttk.Button(mainframe, text="Go",
-#                command=lambda: main(Word_Details(course_name.get(), week.get(),
-#                                                  video_name.get("1.0", END), num_slides.get())))\
-#         .grid(column=3, row=6, sticky=E)
 #
-#     #set up labels for the ui
-#     ttk.Label(mainframe, text="Course Name:").grid(column=1, row=1, sticky=W)
-#     ttk.Label(mainframe, text="Week Number:").grid(column=1, row=2, sticky=W)
-#     ttk.Label(mainframe, text="Video Name:").grid(column=1, row=3, sticky=W)
-#     ttk.Label(mainframe, text="Number of Slides:").grid(column=1, row=5, sticky=W)
-#
-#     for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
-#
-#     course_name_entry.focus()
-#     root.bind('<Return>', main)
-#
-#     root.mainloop()
 
 if __name__ == '__main__':
-    interface = UI()
-    interface.New_Or_Open()
+
+    Menu.New_Or_Open()
 
 # VBA -> Python notes:
 #   ActiveDocument refers to doc in the Word class
